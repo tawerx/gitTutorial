69467f08676f2c8c7aabc9aec3fc8e7d2e25cd90
Дима Бурлаков <tawerxo@mail.ru>
2025-10-06 23:15:10 +0300

    Добавил футер на главную страницу

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import { useState } from 'react'
 import './App.css'
+import { Footer } from './components/Footer'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -18,6 +19,7 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
+      <Footer />
     </>
   )
 }
