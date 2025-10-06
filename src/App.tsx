c74a35ae4cc87f327f3f36e379ca4927dea65e60
Дима Бурлаков <tawerxo@mail.ru>
2025-10-06 23:13:39 +0300

    Добавил хедер на главную страницу

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import { useState } from 'react'
 import './App.css'
+import { Header } from './components/Header'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <h1>Vite + React</h1>
+      <Header />
+      <h1>Vite + React</h1> 
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
