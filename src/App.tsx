3e988263511568081f13c5af45474bad36abecc9
tawerx <tawerxo@mail.ru>
2026-04-01 12:44:34 +0300

    test

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,28 @@
-import { useState } from 'react'
-import './App.css'
+import { useState } from 'react';
+import './App.css';
 
-import { Header } from './components/Header'
-import { Footer } from './components/Footer'
-
+import { Header } from './components/Header';
+import { Footer } from './components/Footer';
+import TextItem from './components/TextItem/TextItem';
 
 function App() {
-  const [count, setCount] = useState(0)
+    const [count, setCount] = useState(0);
 
-  return (
-    <>
-      <Header />
-      <h1>Vite + React</h1> 
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-      <Footer />
-    </>
-  )
+    return (
+        <>
+            <Header />
+            <h1>Vite + React</h1>
+            <div className="card">
+                <button onClick={() => setCount((count) => count + 1)}>count is {count}</button>
+                <p>
+                    Edit <code>src/App.tsx</code> and save to test HMR
+                </p>
+            </div>
+            <TextItem />
+            <p className="read-the-docs">Click on the Vite and React logos to learn more</p>
+            <Footer />
+        </>
+    );
 }
 
-export default App
+export default App;
